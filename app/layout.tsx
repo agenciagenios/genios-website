b81e3ddebba7ab93011f9a5855c12618a58b32af
Raul Rodrigues <raulrodriguesdemoraes@gmail.com>
2025-12-19 06:22:05 -0500

    smooth scroll

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import BackToTop from "./components/BackToTop";
+import SmoothScroll from "./components/SmoothScroll";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -37,6 +38,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased bg-black text-white selection:bg-yellow-500/30`}
       >
+        <SmoothScroll />
         <Header />
         {children}
         <Footer />
